c30b8d7abd62ba68151593cf604c24492cdf925d
dadepu <danielkoellgen@gmail.com>
2021-05-25 09:30:13 +0200

    Initial Commit

---
+++ SRL/SRLApp.swift
@@ -0,0 +1,17 @@
+//
+//  SRLApp.swift
+//  SRL
+//
+//  Created by Daniel Koellgen on 25.05.21.
+//
+
+import SwiftUI
+
+@main
+struct SRLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
